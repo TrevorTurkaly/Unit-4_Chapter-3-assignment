3e4e4cb700d73c012ca54a692aa4c9b082ec44c6
TrevorTurkaly <123902025+TrevorTurkaly@users.noreply.github.com>
2023-02-13 19:47:00 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//this is excercise 4 from the chapter
+#include <iostream>
+
+int main() {
+// create varibles and set their value
+int C = 0;
+int F = 27;
+  
+//create formula
+C = (F-32) / 1.8;
+  
+// show what the ending value of "C" is 
+std::cout<<"Celcius is equal to "<<C<<std::endl;
+  
+}
